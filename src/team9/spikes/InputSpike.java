cc4e91be8fa4ce8abe493cab77e62e453c461373
Jsudz <johnds2112@gmail.com>
2019-03-14 17:24:58 -0300

    Fixed Indents on spike
    Noticed the whole class was indented too far, fixed.

--- src/team9/spikes/InputSpike.java
+++ src/team9/spikes/InputSpike.java
@@ -11,77 +11,76 @@ public class InputSpike{
  * Testing that I can read input from the second sheet of an .xls workbook with titles, a number 
  * in its first row of data and different numbers of strings in the rows beneath, similar to our rank schema.
  */
-		    public static void main(String[] args) {
-		    	final String FILENAME="demo/testsheet.xls";
-		    	try {
-		    		FileInputStream excelFile = new FileInputStream(new File(FILENAME));
-		            Workbook workbook = new HSSFWorkbook(excelFile);
-		            Sheet sheet = workbook.getSheetAt(1);
-		            Iterator<Row> iterator = sheet.iterator();
-		            String[] titles=new String[3];
-		            String[][] courses=new String[3][4];
-		            double[] hours=new double[3];
-		            int counter, courseCounter;
-		            
-		            //getting titles
-	                Row currentRow = iterator.next();
-	                Iterator<Cell> cellIterator = currentRow.iterator();
-	                counter=0;
-	                while (cellIterator.hasNext()) {
-	                    Cell currentCell = cellIterator.next();
-	                    titles[counter]=currentCell.getStringCellValue();
-	                    counter++;
-	                }
-		            
-		            //getting numbers
-	            	currentRow = iterator.next();
-	                cellIterator = currentRow.iterator();
-	                counter=0;
-	               
-	                while (cellIterator.hasNext()) {
-	                    Cell currentCell = cellIterator.next();
-	                    hours[counter]=currentCell.getNumericCellValue();
-	                    counter++;
-	                }
-		            
-		            courseCounter=0;
-		            while(iterator.hasNext()) {//getting courses
-		            	currentRow = iterator.next();
-		                cellIterator = currentRow.iterator();
-		                counter=0;
-		                while (cellIterator.hasNext()) {
-		                    Cell currentCell = cellIterator.next();
-		                    courses[currentCell.getColumnIndex()][courseCounter]=currentCell.getStringCellValue();
-		                    counter++;
-		                }
-		                courseCounter++;
-		            }
-		            System.out.print("Titles:\t");//Printing Results
-		            for(int i=0;i<titles.length;i++) {
-		            	System.out.print(titles[i]+"\t");
-		            }
-		            System.out.println();
-		            
-		            System.out.print("Hours:\t");
-		            for(int i=0;i<hours.length;i++) {
-		            	System.out.print(hours[i]+"\t");
-		            }
-		            System.out.println();
-		            
-		            System.out.println("Courses:\t");
-		            for (int j=0; j<courses[0].length;j++) {
-			            System.out.print("\t");
-		            	for(int i=0;i<courses.length;i++) {
-			            	System.out.print(courses[i][j]+"\t");
-			            }
-			            System.out.println();
-		            }  
-		            workbook.close();
-		        } catch (FileNotFoundException e) {
-		            e.printStackTrace();
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-
+    public static void main(String[] args) {
+    	final String FILENAME="demo/testsheet.xls";
+    	try {
+    		FileInputStream excelFile = new FileInputStream(new File(FILENAME));
+            Workbook workbook = new HSSFWorkbook(excelFile);
+            Sheet sheet = workbook.getSheetAt(1);
+            Iterator<Row> iterator = sheet.iterator();
+            String[] titles=new String[3];
+            String[][] courses=new String[3][4];
+            double[] hours=new double[3];
+            int counter, courseCounter;
+            
+            //getting titles
+            Row currentRow = iterator.next();
+            Iterator<Cell> cellIterator = currentRow.iterator();
+            counter=0;
+            while (cellIterator.hasNext()) {
+                Cell currentCell = cellIterator.next();
+                titles[counter]=currentCell.getStringCellValue();
+                counter++;
+            }
+            
+            //getting numbers
+        	currentRow = iterator.next();
+            cellIterator = currentRow.iterator();
+            counter=0;
+           
+            while (cellIterator.hasNext()) {
+                Cell currentCell = cellIterator.next();
+                hours[counter]=currentCell.getNumericCellValue();
+                counter++;
+            }
+            
+            courseCounter=0;
+            while(iterator.hasNext()) {//getting courses
+            	currentRow = iterator.next();
+                cellIterator = currentRow.iterator();
+                counter=0;
+                while (cellIterator.hasNext()) {
+                    Cell currentCell = cellIterator.next();
+                    courses[currentCell.getColumnIndex()][courseCounter]=currentCell.getStringCellValue();
+                    counter++;
+                }
+                courseCounter++;
+            }
+            System.out.print("Titles:\t");//Printing Results
+            for(int i=0;i<titles.length;i++) {
+            	System.out.print(titles[i]+"\t");
+            }
+            System.out.println();
+            
+            System.out.print("Hours:\t");
+            for(int i=0;i<hours.length;i++) {
+            	System.out.print(hours[i]+"\t");
+            }
+            System.out.println();
+            
+            System.out.println("Courses:\t");
+            for (int j=0; j<courses[0].length;j++) {
+	            System.out.print("\t");
+            	for(int i=0;i<courses.length;i++) {
+	            	System.out.print(courses[i][j]+"\t");
+	            }
+	            System.out.println();
+            }  
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 }
