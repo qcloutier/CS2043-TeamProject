b7578b074822f6f3ec653e7043fbb41815be75ad
Retreever <47255780+Retreever@users.noreply.github.com>
2019-03-28 08:17:31 -0300

    Raw Distribution calculation method - first pass not complete

--- src/team9/transcriptanalyzer/output/RawDistribution.java
+++ src/team9/transcriptanalyzer/output/RawDistribution.java
@@ -21,8 +21,34 @@ public class RawDistribution extends Distribution{
 		entries.add(new RawEntry(course, values));
 	}
 	
-	public void calculate(Configuration config, Cohort cohort) {
+	/*public void calculate(Configuration config, Cohort cohort) {
 		//TODO
+	}*/
+	
+	public void calculate(Configuration config, Cohort cohort) {
+		ArrayList<RawDist> list = new ArrayList<RawDist>();
+		int other = 0;
+		int fails = 0;
+		int marginal = 0;
+		int meets = 0;
+		int exceeds = 0;
+		
+		
+		for(int i = 0; i < config.courses.length(); i++) {
+			
+			for(int j = 0; j < cohort.transcripts.length(); j++)
+			
+			if(config.courses[i] = cohort.transcripts[j]) {
+				other++;
+				fails++;
+				marginal++;
+				meets++;
+				exceeds++;
+				
+			}
+			list.add(new RawDist(config[i],other,fails,marginal,meets,exceeds));
+			
+		}
 	}
 	
 	private class RawEntry{
