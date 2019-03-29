c7ddcc4a97b6c5a3a9570067d2c4fbe9733bb9a6
Mark <mrkhlt422@gmail.com>
2019-03-29 14:33:33 -0300

    Work done in meeting on the 29th of march

--- src/team9/transcriptanalyzer/output/RawDistribution.java
+++ src/team9/transcriptanalyzer/output/RawDistribution.java
@@ -26,37 +26,74 @@ public class RawDistribution extends Distribution{
 	}*/
 	
 	public void calculate(Configuration config, Cohort cohort) {
-		ArrayList<RawDist> list = new ArrayList<RawDist>();
-		int other = 0;
-		int fails = 0;
-		int marginal = 0;
-		int meets = 0;
-		int exceeds = 0;
-		
-		
-		for(int i = 0; i < config.courses.length(); i++) {
-			
-			for(int j = 0; j < cohort.transcripts.length(); j++)
-			
-			if(config.courses[i] = cohort.transcripts[j]) {
-				other++;
-				fails++;
-				marginal++;
-				meets++;
-				exceeds++;
-				
-			}
-			list.add(new RawDist(config[i],other,fails,marginal,meets,exceeds));
-			
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		for(int i = 0; i < 5; i++) {
+			values.add(0);
 		}
+		
+		List<String> courses = cohort.getMasterList();
+		List<Transcript> transcripts = cohort.getTranscripts();
+		
+		GradeSchema gradeSchema = config.getGradeSchema();
+		
+		for(String course : courses) {
+			
+			for(Transcript transcript : transcripts) {
+				
+				List<TranscriptCourse> transcriptCourses = transcript.getCourses();
+				for(TranscriptCourse transcriptCourse : transcriptCourses) {
+					
+					if(transcriptCourse.getID().equals(course) || checkEquivalents(transcriptCourse, course)) {
+						Grade grade = transcriptCourse.getGrade();
+						if(grade.asPoint() >= gradeSchema.getLower("exceeds").asPoint()) {
+							values.set(4, values.get(4) + 1);
+						}
+						if(grade.asPoint() >= gradeSchema.getLower("meets").asPoint()) {
+							values.set(3, values.get(3) + 1);
+						}
+						if(grade.asPoint() >= gradeSchema.getLower("marginal").asPoint()) {
+							values.set(2, values.get(2) + 1);
+						}
+						if(grade.asPoint() >= gradeSchema.getLower("fails").asPoint()) {
+							values.set(1, values.get(1) + 1);
+						}
+						else {
+							values.set(0, values.get(0) + 1);
+						}
+					}
+				}
+			}
+			
+			entries.add(new RawEntry(course, values));
+			for(int i = 0; i < 5; i++) {
+				values.set(i, 0);
+			}
+		}
+	}
+	
+	public String[][] getRawDistribution(){
+		
+		String [][] rawDistributions = new String[entries.size()][6];
+		
+		for(int i = 0; i < entries.size(); i++) {
+			
+			RawEntry name = entries.get(i);
+			
+			for(int j = 1; j < 6; j++) {
+				
+				rawDistributions[1]
+			}
+		}
+		
+		return rawDistributions;
 	}
 	
 	private class RawEntry{
 		
-		public CourseEquivalents course;
+		public String course;
 		public List<Integer> values;
 		
-		public RawEntry(CourseEquivalents course, List<Integer> values) {
+		public RawEntry(String course, List<Integer> values) {
 			this.course = course;
 			this.values = values;
 			values = new ArrayList<Integer>();
