1e882285371190cd58f4dd689675e4aaf464b578
qcloutier <quintoncloutier@outlook.com>
2019-04-01 14:46:55 -0300

    Listing areas and equivalents
    Added methods to CourseAreas and CourseEquivalents for creating a double
    list of this stuff for easier writing to a file.

--- src/team9/transcriptanalyzer/input/CourseAreas.java
+++ src/team9/transcriptanalyzer/input/CourseAreas.java
@@ -1,10 +1,14 @@
 package team9.transcriptanalyzer.input;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
  * Defines the course areas from the configuration file.
@@ -40,6 +44,53 @@ public class CourseAreas {
 	 */
 	public List<String> getAreas(String course) {
 		return this.areas.get(course);
+	}
+	
+	/**
+	 * Generates a double list of all course areas.
+	 * @return A double list of all course areas.
+	 */
+	public List<ArrayList<String>> listAllAreas() {
+		
+		List<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
+		
+		Iterator<String> keys = areas.keySet().iterator();
+		while (keys.hasNext()) {
+			
+			String child = keys.next();
+			List<String> parents = areas.get(child);
+			for (String parent : parents) {
+	
+				boolean exists = false;
+				int c=0;
+				while (c<results.size()) {
+					if (results.get(c).get(0).equals(parent)) {
+						exists = true;
+						break;
+					}
+					c++;
+				}
+				
+				if (exists) {
+					results.get(c).add(child);
+				}
+				else {
+					ArrayList<String> temp = new ArrayList<String>();
+					temp.add(parent);
+					temp.add(child);
+					results.add(temp);
+				}
+			}
+		}
+		
+		return results;
+	}
+	
+	public static void main(String[]args) throws Exception {
+		
+		Workbook wb = WorkbookFactory.create(new File("demo/IO Spec Input.xlsx"));
+		
+		System.out.println(ExcelReader.parseAreas(wb.getSheet("Course Areas")).listAllAreas());
 	}
 	
 	public String toString() {
